74864c967fb97af0a1dfea67308fbe1676d66a9f
ikerp <ikerperezcarcamo1921@gmail.com>
2021-03-13 11:43:57 +0100

    atributo nombreOrdenador pasa a ser privado

--- gitNuevo/src/gitNuevo/Ordenador.java
+++ gitNuevo/src/gitNuevo/Ordenador.java
@@ -2,8 +2,8 @@ package gitNuevo;
 
 public class Ordenador {
 
-	public String nombreOrdenador;
-	
+	private String nombreOrdenador;
+		
 	public Ordenador(String nombreOrdenador){
 		this.nombreOrdenador=nombreOrdenador;
 	}
